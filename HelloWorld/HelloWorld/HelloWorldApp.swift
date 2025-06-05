2a67db3a30a4b9589e2fc875be9d8698a43df736
Den <anatoley-fedorov@yandex.ru>
2025-06-05 21:39:25 +0700

    Обернули все отладочные принты в в #if DEBUG

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Anatoly Fedorov on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
